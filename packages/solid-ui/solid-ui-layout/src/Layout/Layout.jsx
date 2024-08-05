ca59fdc67f98d1858a145e4e7e36379327422e61
Alessio Gega <144816213+alessiogegaa@users.noreply.github.com>
2024-08-05 09:42:13 +0200

    Update Layout.jsx

--- packages/solid-ui/solid-ui-layout/src/Layout/Layout.jsx
+++ packages/solid-ui/solid-ui-layout/src/Layout/Layout.jsx
@@ -13,7 +13,7 @@ const Layout = ({ children, pageContext = {}, location, theme = {} }) => {
   return (
     <ThemeProvider theme={merge(baseTheme, theme)}>
       <Helmet>
-      <script type="text/javascript" id="hs-script-loader" async defer src="//js-eu1.hs-scripts.com/143605925.js"></script>
+      <script type="text/javascript" id="zsiqchat">var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode: "siq9f259ee8d693afdd5a7a2dec6b0e2f3c4ef134270b0355c636866043c274a28d", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zohopublic.eu/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);</script>
       </Helmet>
       <pageContextProvider.Provider value={{ pageContext, location }}>
         <FormContextProvider>
